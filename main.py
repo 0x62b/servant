a6e38649a6e874b496e9e3561dbbfd124c9e88ee
Benjamin Yang <benjamin.yang12@outlook.com>
2025-11-14 14:59:26 +1100

    reminder command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import time
 from slack_bolt import App
 from slack_bolt.adapter.flask import SlackRequestHandler
 from flask import Flask, jsonify, request
@@ -51,8 +52,63 @@ def cmd_test():
         "response_type": "ephemeral",
         "text": f"User ID: {user_id}\nChannel ID: {channel_id}"
     }
-    
+
     return jsonify(response)
+
+@flask.route("/commands/reminder", methods=["POST"])
+def cmd_reminder():
+    data = request.form
+    user_id = data.get('user_id')
+    
+    if user_id != "U092839T3A7":
+        res = {
+            "response_type": "ephemeral",
+            "text": "pls stop trying to gaslight me into thinking i have shit to do\n\nyou dont have perms to use this cmd"
+        }
+
+        return jsonify(res)
+
+    channel_id = data.get('channel_id')
+    text = data.get('text', '').split()
+
+    if len(text) != 2:
+        res = {
+            "response_type": "ephemeral",
+            "text": "noooo u need two argument"
+        }
+        return jsonify(res)
+    
+    reminder = text[0]
+    minutes = text[1]
+    
+    try:
+        minutes = int(minutes)
+        post_at = int(time.time()) + (minutes * 60)
+        
+        app.client.chat_scheduleMessage(
+            channel=channel_id,
+            post_at=post_at,
+            text=f"<@U092839T3A7> reminder: u got shit to do: {reminder}"
+        )
+    except ValueError:
+        res = {
+            "response_type": "ephemeral",
+            "text": "time needs to be a number in mins"
+        }
+        return jsonify(res)
+    
+    except Exception as e:
+        res = {
+            "response_type": "ephemeral",
+            "text": f"error: {str(e)}"
+        }
+        return jsonify(res)
+        
+    res = {
+        "response_type": "ephemeral",
+        "text": "ok"
+    }
+    return jsonify(res)
 
 if __name__ == "__main__":
     flask.run(host="0.0.0.0", port=3001)
