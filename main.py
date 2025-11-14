720d56822ab1d82b4e4ba68c202252d4b18c8d14
Benjamin Yang <benjamin.yang12@outlook.com>
2025-11-14 15:49:23 +1100

    seperate commands into seperate file

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from slack_bolt.adapter.flask import SlackRequestHandler
 from flask import Flask, jsonify, request
 from dotenv import load_dotenv
 
+from commands import Commands
+
 load_dotenv()
 
 app = App(
@@ -58,58 +60,8 @@ def cmd_test():
 @flask.route("/commands/reminder", methods=["POST"])
 def cmd_reminder():
     data = request.form
-    user_id = data.get('user_id')
     
-    if user_id != "U092839T3A7":
-        res = {
-            "response_type": "ephemeral",
-            "text": "pls stop trying to gaslight me into thinking i have shit to do\n\nyou dont have perms to use this cmd"
-        }
-
-        return jsonify(res)
-
-    channel_id = data.get('channel_id')
-    text = data.get('text', '')
-    split = text.split()
-
-    if len(split) < 2:
-        res = {
-            "response_type": "ephemeral",
-            "text": "noooo u need at least two argument"
-        }
-        return jsonify(res)
+    Commands.reminder(data, app)
     
-    minutes = split[-1]
-    reminder = text.removesuffix(minutes).strip()
-    
-    try:
-        minutes = int(minutes)
-        post_at = int(time.time()) + (minutes * 60)
-        
-        app.client.chat_scheduleMessage(
-            channel=channel_id,
-            post_at=post_at,
-            text=f"<@U092839T3A7> reminder: u got shit to do: {reminder}"
-        )
-    except ValueError:
-        res = {
-            "response_type": "ephemeral",
-            "text": "time needs to be a number in mins"
-        }
-        return jsonify(res)
-    
-    except Exception as e:
-        res = {
-            "response_type": "ephemeral",
-            "text": f"error: {str(e)}"
-        }
-        return jsonify(res)
-        
-    res = {
-        "response_type": "ephemeral",
-        "text": f"ok, will remind '{reminder}' in {minutes} minutes"
-    }
-    return jsonify(res)
-
 if __name__ == "__main__":
     flask.run(host="0.0.0.0", port=3001)
